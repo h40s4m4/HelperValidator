04d403cdcb34856715ee4cb3c8b99a9901f527d8
hernan <hernan.bozan@gmail.com>
2023-10-14 21:11:04 +1300

    Update for Symfony request service

--- src/Symfony/SymfonyRequestHelperService.php
+++ src/Symfony/SymfonyRequestHelperService.php
@@ -12,62 +12,55 @@ use function sprintf;
 
 final class SymfonyRequestHelperService
 {
+    private const GET_POST_REQUEST_MESSAGE = 'El campo [%s] no se encuentra definido dentro del Request';
+
     /**
-     * Fetch an element from the request post based on the supplied KEY. If the KEY does not exist, it returns NULL.
+     * Gets an element from the request (based on POST) with the specified KEY. If the KEY does not exist, it returns NULL.
      *
      * @param Request $request
      * @param string  $key
      * @param bool    $hardException
      *
-     * @return string|int|bool|float|null
+     * @return string|int|bool|float|array|null
      *
      * @throws InvalidArgumentException In case the value of the request does NOT have the KEY and the flag $hardException = true.
      */
-    public static function getElementFromPostRequest(Request $request, string $key, bool $hardException = FALSE): string|int|bool|float|null
+    public static function getFromPostRequest(Request $request, string $key, bool $hardException = FALSE): string|int|bool|float|array|null
     {
         if ($request->request->has($key)) {
             return $request->request->get($key);
         }
 
         if (TRUE === $hardException) {
-            throw new InvalidArgumentException(
-                sprintf('El campo [%s] no se encuentra definido dentro del Request', $key),
-            );
+            throw new InvalidArgumentException(sprintf(self::GET_POST_REQUEST_MESSAGE, $key));
         }
 
         return NULL;
     }
 
     /**
-     * Gets an element from the request based on the KEY.
-     * The Elements come from the DATA field of the request (if DATA does not come within the request, it will always throw an exception).
+     * Gets an element from the request (based on POST) with the specified KEY.
+     * The Elements come from the DATA field of the request (if DATA doesn't come in the request, it will always throw an exception).
      * If the KEY does not exist, it returns NULL.
      *
      * @param Request $request
      * @param string  $key
      * @param bool    $hardException
      *
-     * @return string|int|bool|float|null
+     * @return string|int|bool|float|array|null
      *
+     * @throws InvalidArgumentException In case the value of the request doesn't contains 'data'.
      * @throws InvalidArgumentException In case the value of the request does NOT have the KEY and the flag $hardException = true.
      */
-    public static function getElementFromPostRequestByData(Request $request, string $key, bool $hardException = FALSE): string|int|bool|float|null
+    public static function getFromPostRequestByData(Request $request, string $key, bool $hardException = FALSE): string|int|bool|float|array|null
     {
-        // Check 'data' value.
-        if (! $request->request->has('data')) {
-            throw new InvalidArgumentException('Data no se encuentra definido dentro del Request');
-        }
-
-        // Check key in arrayData, and return if exists.
-        $dataArray = $request->get('data');
+        $dataArray = self::getFromPostRequest($request, 'data', TRUE);
         if (ArrayValidator::checkIfKeyExists($dataArray, $key)) {
             return $dataArray[$key];
         }
 
         if (TRUE === $hardException) {
-            throw new InvalidArgumentException(
-                sprintf('El campo [%s] no se encuentra definido dentro del Request', $key),
-            );
+            throw new InvalidArgumentException(sprintf(self::GET_POST_REQUEST_MESSAGE, $key));
         }
 
         return NULL;
